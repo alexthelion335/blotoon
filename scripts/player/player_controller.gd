792dd95abbe4b40ca18556f71db72f0f682a9007
Alex Kinch <alexthelion335@gmail.com>
2026-06-20 00:56:19 -0400

    Movement working for the most part

--- scripts/player/player_controller.gd
+++ scripts/player/player_controller.gd
@@ -5,7 +5,7 @@ extends CharacterBody3D
 @export var acceleration: float = 25.0
 @export var friction: float = 20.0
 @export var jump_force: float = 8.0
-@export var gravity: float = -9.8
+@export var gravity: float = 9.8
 
 var is_sprinting: bool = false
 var camera_controller: Node3D
@@ -15,32 +15,46 @@ func _ready() -> void:
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
 func _physics_process(delta: float) -> void:
-	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	is_sprinting = Input.is_action_pressed("ui_select")
-
+	var input_x = 0.0
+	var input_z = 0.0
+	
+	if Input.is_key_pressed(KEY_W):
+		input_z -= 1.0
+	if Input.is_key_pressed(KEY_S):
+		input_z += 1.0
+	if Input.is_key_pressed(KEY_A):
+		input_x -= 1.0
+	if Input.is_key_pressed(KEY_D):
+		input_x += 1.0
+	
+	var input_dir = Vector2(input_x, input_z).normalized()
+	is_sprinting = Input.is_key_pressed(KEY_SHIFT)
+	
 	var target_speed = sprint_speed if is_sprinting else move_speed
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-
-	if direction:
+	
+	if direction.length() > 0:
 		velocity.x = lerp(velocity.x, direction.x * target_speed, acceleration * delta)
 		velocity.z = lerp(velocity.z, direction.z * target_speed, acceleration * delta)
 	else:
 		velocity.x = lerp(velocity.x, 0.0, friction * delta)
 		velocity.z = lerp(velocity.z, 0.0, friction * delta)
-
+	
 	if is_on_floor():
-		if Input.is_action_just_pressed("ui_accept"):
+		if Input.is_key_pressed(KEY_SPACE):
 			velocity.y = jump_force
 		else:
-			velocity.y += gravity * delta
-
-		move_and_slide()
+			velocity.y = 0.0
+	else:
+		velocity.y -= gravity * delta
+	
+	move_and_slide()
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
 		rotate_y(-event.relative.x * 0.005)
-	if camera_controller:
-		camera_controller.rotate_camera(event.relative.y * 0.005)
-
+		if camera_controller:
+			camera_controller.rotate_camera(event.relative.y * 0.005)
+	
 	if Input.is_action_just_pressed("ui_cancel"):
 		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE if Input.mouse_mode == Input.MOUSE_MODE_CAPTURED else Input.MOUSE_MODE_CAPTURED
